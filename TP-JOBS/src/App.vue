a5578d27ead393a49d26099ff9929d45c4a148e9
ELANSARI05 <149951748+ELANSARI05@users.noreply.github.com>
2024-05-11 13:00:32 +0100

    Update App.vue

--- TP-JOBS/src/App.vue
+++ TP-JOBS/src/App.vue
@@ -1,13 +1,27 @@
 <template>
-    <RouterView />
+    <div>
+    <router-link @click="tohome()" v-show="a" to="/Home">Home</router-link> 
+    </div>
+  <router-view/>
 </template>
 
 <script>
+
 export default {
 name: 'App',
 components :{
- 
+   
 },
+data(){
+  return {
+    a : true
+  }
+},
+methods : {
+  tohome(){
+    this.a = false
+  }
+}
 }
 </script>
 
@@ -20,4 +34,12 @@ components :{
   color: #2c3e50;
   margin-top: 60px;
 }
+.title {
+  font-size: 40px; /* Adjust the font size as needed */
+  font-weight: bold;
+  color: #333; /* Choose a suitable color */
+  text-transform: uppercase; /* Optional: Convert text to uppercase */
+  margin-bottom: 10px; /* Optional: Add margin for spacing */
+}
+
 </style>
