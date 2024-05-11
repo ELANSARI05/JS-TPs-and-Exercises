f0da55aafee69777040393e8f6600dcefddf1893
ELANSARI05 <149951748+ELANSARI05@users.noreply.github.com>
2024-05-11 13:04:14 +0100

    Update SHOW.vue

--- TP-JOBS/src/components/SHOW.vue
+++ TP-JOBS/src/components/SHOW.vue
@@ -1,9 +1,11 @@
 <template>
+    <tr v-for=" job in jobs" >
     <th @click="show_details(job)"><b>{{job.id}}</b></th>
-    <th @click="show_details(job)"><b>{{job.titre}}</b></th>
-    <td @click="show_details(job)">{{job.description}}</td>
-    <th><button class="button_det" @click="job_edit(job)">Edit</button></th>
-    <th><button class="button_det" @click="job_delete(job)">Delete</button></th>
+    <th><b>{{job.titre}}</b></th>
+    <td>{{job.description}}</td>
+    <th><router-link :to="{ name: 'EDIT', params: { id: job.id } }" class="no-underline"><button class="button_det" >Edit</button></router-link></th>
+    <th><button class="button_det1" @click="job_delete(job)">Delete</button></th>
+    </tr>
 </template>
   
 <script>
@@ -13,29 +15,24 @@ export default {
     return {
     jobs: []
     }},
-  methods: {
-    show_details(job) {
-        this.$router.push({ name: 'JOB_DETAIL', params: { id: job.id } }); 
-    },
-    job_edit(job) {
-        this.$router.push({ name: 'EDIT', params: { id: job.id } });
-    },
-    job_delete(job) {
-            fetch('http://localhost:3000/emplois/' + job.id, {
-                method: 'DELETE'
-            })
-            .then(() => this.jobs = this.jobs.filter(job1 => job1.id !== job.id))
-            .catch(error => console.log(error.message))
-
-        }
-  },
   mounted(){
-        fetch('http://localhost:3000/emplois')
+        fetch('http://localhost:4000/emplois')
         .then(response => response.json())
         .then(data => this.jobs = data)
         .catch(error => console.log(error.message))
     },
-  props: ["job"],
+  methods: {
+    show_details(job) {
+        this.$router.push({ name: 'JOB_DETAIL', params: { id: job.id } }); 
+    },
+    job_delete(job) {
+            fetch('http://localhost:4000/emplois/' + job.id, {
+                method: 'DELETE'
+            })
+            .then(() => this.jobs = this.jobs.filter(job1 => job1.id !== job.id))
+            .catch(error => console.log(error.message))
+        }
+  },
 }
 
 
@@ -52,6 +49,15 @@ export default {
   }
   .button_det {
   background-color: #3498db; /* Blue color */
+  color: #fff; /* White text */
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+}
+.button_det1 {
+  background-color: red; /* Blue color */
   color: #fff; /* White text */
   border: none;
   padding: 10px 20px;
