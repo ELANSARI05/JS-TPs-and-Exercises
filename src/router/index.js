1c5a0dec799ff9860297c3ecc9502a2e44a0546e
Mohamedaymanbourich <Mohamed.BOURICH@um6p.ma>
2024-05-20 10:59:18 +0100

    yes

--- src/router/index.js
+++ src/router/index.js
@@ -5,8 +5,38 @@ import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
 import Profile from '../views/Profile.vue'
 import Register from '../views/Register.vue'
-const routes = [
 
+const routes = [
+  {
+    path: '/home',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/admin',
+    name: 'Admin',
+    component: Admin
+  },
+  {
+    path: '/discussion',
+    name: 'Discussion',
+    component: Discussion
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: Register
+  }
 ]
 
 const router = createRouter({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
-  
+    <div class="container">
 
-  
+    <router-link to="/discussion" class="d1">Discussion 1</router-link> <br> <br>
+    <router-link to="/discussion" class="d2">Discussion 2</router-link> <br> <br>
+    <router-link to="/discussion" class="d3">Discussion 3</router-link> <br> <br>
+
+    </div>
+
 </template>
 
 <script>
@@ -17,5 +22,10 @@ export default {
 </script>
 
 <style>
+.d1,.d2,.d3{
+text-decoration : none
 
+
+
+}
 </style>
